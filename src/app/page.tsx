b7ec7873aef7ff17fe697a627246f1ae8f285985
James Armbrust <james.armbrust@gmail.com>
2025-02-26 12:53:10 -0700

    basic table and info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,31 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <main className="flex flex-col justify-center items-center">
+        <h1 className="flex mt-[20px] text-2xl">Hello, and welcome to my humble website!</h1>
+        <p className="mt-[10px]">My name is James Armbrust, and I am a software developer based in sunny Colorado!</p>
+        <p className="mt-[10px]">The purpose of this simple page is to collect some of my recent personal projects in one place.</p>
+        <p className="mt-[10px]">I will add projects here as I create suitable ones, and improving on this page as I go, so stay tuned!</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="mt-[30px]">
+          <table className="w-full border-collapse">
+            <tbody className="border border-zinc-500">
+              <tr className="border border-zinc-500" ><th className="px-4 py-2">Project name</th><th className="px-4 py-2">Description</th><th className="px-4 py-2">Link</th></tr>
+              <tr className="border border-zinc-500">
+                <td className="px-4 py-2">Hire an Adventurer!</td>
+                <td className="max-w-[600px] px-4 py-2">
+                  A simple e-commerce site in the guise of an adventure game.  Your goal is to protect your village by hiring adventurers to slay the monsters attacking it.  You will have a limited amount of currency to hire them, and a limited pool of adventurers to draw from.  The e-commerce aspect is that the you have a cart you can add and remove &quot;adventurers&quot; from before you choose to hire them. And the stock of each adventurer can be exhausted if adventurers are slain on any of their missions.
+                </td>
+                <td><a className="text-blue-700 px-4 py-2" href="https://hire-an-adventurer.vercel.app/">hire-an-adventurer</a></td>
+              </tr>
+              <tr className="border border-zinc-500" ><td className="px-4 py-2">In Progress...</td><td className="max-w-[600px] px-4 py-2">TBD</td><td className="px-4 py-2">TBD</td></tr>
+            </tbody>
+          </table>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="left-8 text-sm absolute bottom-4">
+        &copy;2025 James Armbrust
       </footer>
     </div>
   );
