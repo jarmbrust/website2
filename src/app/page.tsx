bf040639bb0b6b7d573687c30d8c7d8009fd4425
James Armbrust <james.armbrust@gmail.com>
2025-02-26 13:02:12 -0700

    blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                 <td className="max-w-[600px] px-4 py-2">
                   A simple e-commerce site in the guise of an adventure game.  Your goal is to protect your village by hiring adventurers to slay the monsters attacking it.  You will have a limited amount of currency to hire them, and a limited pool of adventurers to draw from.  The e-commerce aspect is that the you have a cart you can add and remove &quot;adventurers&quot; from before you choose to hire them. And the stock of each adventurer can be exhausted if adventurers are slain on any of their missions.
                 </td>
-                <td><a className="text-blue-700 px-4 py-2" href="https://hire-an-adventurer.vercel.app/">hire-an-adventurer</a></td>
+                <td><a className="text-blue-700 px-4 py-2" href="https://hire-an-adventurer.vercel.app/" target="_blank">hire-an-adventurer</a></td>
               </tr>
               <tr className="border border-zinc-500" ><td className="px-4 py-2">In Progress...</td><td className="max-w-[600px] px-4 py-2">TBD</td><td className="px-4 py-2">TBD</td></tr>
             </tbody>
