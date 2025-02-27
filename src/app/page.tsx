4460c8a103e630bee139ade6cab030866cc1af66
James Armbrust <james.armbrust@gmail.com>
2025-02-27 12:38:15 -0700

    small refactor and rewording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,43 @@
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col justify-center items-center">
+      <main className="flex flex-col justify-center items-center px-4 py-4">
         <h1 className="flex mt-[20px] text-2xl">Hello, and welcome to my humble website!</h1>
         <p className="mt-[10px]">My name is James Armbrust, and I am a software developer based in sunny Colorado!</p>
         <p className="mt-[10px]">The purpose of this simple page is to collect some of my recent personal projects in one place.</p>
         <p className="mt-[10px]">I will add more projects as I create suitable ones, so stay tuned!</p>
 
         <div className="mt-[30px]">
-          <table className="w-full border-collapse">
+          <table className=" border-collapse">
             <tbody className="border border-zinc-500">
-              <tr className="border border-zinc-500" ><th className="px-4 py-2">Project name</th><th className="px-4 py-2">Description</th><th className="px-4 py-2">Link</th></tr>
-              <tr className="border border-zinc-500">
-                <td className="px-4 py-2">Hire an Adventurer!</td>
-                <td className="max-w-[600px] px-4 py-2">
-                  A simple e-commerce site in the guise of an adventure game.  Your goal is to protect your village by hiring adventurers to slay the monsters attacking it.  You will have a limited amount of currency to hire them, and a limited pool of adventurers to draw from.  The e-commerce aspect is that the you have a cart you can add and remove &quot;adventurers&quot; from before you choose to hire them. And the stock of each adventurer can be exhausted if adventurers are slain on any of their missions.
-                </td>
-                <td><a className="text-blue-700 px-4 py-2" href="https://hire-an-adventurer.vercel.app/" target="_blank">hire-an-adventurer</a></td>
+              <tr className="border border-zinc-500" >
+                <th className="px-4 py-2 border-zinc-500">Project</th>
+                <th className="px-4 py-2 border-zinc-500">Description</th>
+                <th className="px-4 py-2 border-zinc-500">Code</th>
               </tr>
-              <tr className="border border-zinc-500" ><td className="px-4 py-2">In Progress...</td><td className="max-w-[600px] px-4 py-2">TBD</td><td className="px-4 py-2">TBD</td></tr>
+              <tr className="border border-zinc-500">
+                <td className="px-5 py-4 border-zinc-500">
+                  <a
+                    className="text-blue-700"
+                    href="https://hire-an-adventurer.vercel.app/"
+                    target="_blank">
+                    Hire an Adventurer!
+                  </a>
+                </td>
+                <td className="max-w-[600px] px-5 py-4">
+                  Similar to a simple e-commerce site this is an adventure game where you hire heroes to defeat monsters
+                  and save your village.
+                </td>
+                <td className="text-blue-700 px-5 py-4">
+                  <a  href="https://github.com/jarmbrust/hire-an-adventurer" target="_blank">
+                    GitHub
+                  </a>
+                </td>
+              </tr>
+              <tr className="border border-zinc-500" >
+                <td className="px-5 py-4">In Progress...</td>
+                <td className="max-w-[600px] px-5 py-4">TBD</td>
+                <td className="px-5 py-4">TBD</td></tr>
             </tbody>
           </table>
         </div>
